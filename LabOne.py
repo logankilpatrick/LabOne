ac5a9cb8d7453bdb78ebe33b3b918f36b2f02bb1
Logan Kilpatrick <23kilpatrick23@gmail.com>
2018-01-18 11:43:13 -0800

    Initial commit

---
+++ LabOne.py
@@ -0,0 +1,14 @@
+#LabOne
+#CIS41A
+#09/24/17
+#
+#Instruction:
+#Your name
+#The programming language(s) that have you worked with before
+#Something you'd like me to know about you
+
+
+print("Logan Kilpatrick\n")
+print("Ive taken CIS22A, 22B, 22C, 35A, 40, and this quarter 41A and 21JA.  I have a strong background in Java, C++, and Python.")
+print("Outside of De Anza I took an intro course that covered HTML, CSS, and Java Script as well as a graduate level Andriod App Dev class from Carnegie Mellon in Java.\n")
+print("I moved to California last fall from Chicago to attend De Anza after getting wait listed at Berkeley out of high sxhool and ultimatly not gettng in.  I want to transfer into Berkeley next fall for C.S.  I am passionate about Computer Science, School, and Reading!")
